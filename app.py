3358f5bafa98d0ff617c2693b79beba6d67f1e56
Paulo Jonas <70636527+pjonas21@users.noreply.github.com>
2020-09-13 10:51:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,106 @@
+from uuid import uuid4
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from main import Blockchain
+
+app = Flask(__name__)
+
+# gera um edereco global para esse no
+node_identifier = str(uuid4()).replace('-', '')
+
+# inicia a blockchain
+blockchain = Blockchain()
+
+@app.route("/")
+def hello():
+    return "Bem vindo ao S4Coin"
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # executa o algoritmo da prova de trabalho para obter a proxima
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    # recompensa por achar a prova
+    # sender=0 para mostrar que o bloco foi minerado
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # cria um novo bloco para adicionar a cadeia
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    response = {
+        'message': "New Block Added",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our Chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list  of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New Nodes Have Been Added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # verifica se os campos exigidos estao nos dados postados
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return "Missing Values", 400
+
+    # cria uma nova transacao
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ main.py
@@ -0,0 +1,179 @@
+import hashlib
+import json
+from time import time
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+from ecdsa import SigningKey, NIST384p
+
+from flask import Flask, jsonify, request
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set() # usado para manter a lista de nós
+
+        # criacao do primeiro bloco
+        self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        Inclui um novo no na lista
+        :param address: endereco do no. Ex. 'http://192.168.0.3:5000'
+        """
+
+        parsed_url = urlparse(address)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            # aceita um endereco fora do padrao '192.168.0.3:5000'.
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Endereço inválido')
+
+    def valid_chain(self, chain):
+        """
+        Valida a blockchain
+
+        :param chain: <list> Blockchain
+        :return: <bool> Verdadeiro se válido, caso não, Falso
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n--------\n")
+            # verifica se o hash do bloco esta correto
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # verifica a prova de trabalho
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        Faz a substituição da blockchain mais curta
+        pela mais longa na rede.
+
+        :return: <bool> caso verdadeiro, a blackchain foi substituida
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # busca uma corrent mais longa que a atual
+        max_length = len(self.chain)
+
+        # verifica as blockchains de todos os nós na rede
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # verifica o tamanho da blockchain e se ela e valida
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # subsitui a blockchain pela mais longa
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        cria um novo bloclo
+
+        :param proof: <int> prova de trabalho
+        :param previous_hash: (Optional) <str> hash do bloco anterior
+        :return: <dict> novo bloco
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reseta a atual lista de transacoes
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        cria uma nova transação para ir para o próximo bloco
+        :param sender: <str> endereco do remetente
+        :param recipient: <str> endereco de destino
+        :param amount: <int> quantidade
+        :return: <int> indice do bloco responsavel pela transacao
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        """
+        cria o hash do bloco e ordena os hashes para que nao haja inconsistencia
+        :param block: <dict> Bloco
+        :return: <str>
+
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        # obtem o ultimo bloco da blockchain
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        funcao de prova de trabalho:
+        - encontra um numero x, onde o hash(xx') possui 4 zeros a esquerda'
+        - x e a prova anterior e x' e a nova
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Valida a prova de trabalho, certificando que existem 4 zeros anteriores
+
+        :param last_proof: <int> Prova Anterior
+        :param proof: <int> Prova Atual
+        :return: <bool> True se estiver correto, caso não False
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
